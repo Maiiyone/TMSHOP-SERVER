8e27717265c2e6b390856dae43d29150c14e6c05
Tên của bạn <21522322@gm.uit.edu.vn>
2024-11-28 23:01:56 +0700

    okk

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
-  "http://localhost:5174",
+  "http://localhost:5173",
   "https://tmshop-client.vercel.app"
 ];
 
